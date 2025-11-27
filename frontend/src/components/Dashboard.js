03e43db646397ddb73543826ce272819cef6370a
patrickmcsweeney81 <patrick.mcsweeney81@gmail.com>
2025-11-27 17:42:32 +0800

    fix: remove unused API_URL import from Dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import API_URL from '../config';
 import './Dashboard.css';
 
 function Dashboard({ username, onLogout }) {
